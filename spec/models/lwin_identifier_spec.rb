6a2da23e4098a33f46ef8f91f93831d344fc811e
Graeme Mathieson <mathie@woss.name>
2015-01-13 19:23:49 +0000

    Basic model for L-Win identifiers.

---
+++ spec/models/lwin_identifier_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe LwinIdentifier do
+  def factory(attributes = {})
+    described_class.new({
+      identifier: 1000001,
+      status: :live,
+      identifier_updated_at: Date.new(2011, 6, 8),
+      wine: Wine.new(name: 'Reisling')
+    }.merge(attributes))
+  end
+
+  describe 'validations' do
+    it 'requires an identifier' do
+      wine = factory(identifier: '')
+
+      expect(wine).not_to be_valid
+      expect(wine.errors[:identifier]).to include(/can't be blank/)
+    end
+
+    it 'requires a status' do
+      wine = factory(status: '')
+
+      expect(wine).not_to be_valid
+      expect(wine.errors[:status]).to include(/can't be blank/)
+    end
+
+    it 'requires an updated date' do
+      wine = factory(identifier_updated_at: '')
+
+      expect(wine).not_to be_valid
+      expect(wine.errors[:identifier_updated_at]).to include(/can't be blank/)
+    end
+
+    it 'requires a wine if the status is live' do
+      wine = factory(status: :live, wine: nil)
+
+      expect(wine).not_to be_valid
+      expect(wine.errors[:wine]).to include(/can't be blank/)
+    end
+
+    it 'requires a wine if the status is combined' do
+      wine = factory(status: :combined, wine: nil)
+
+      expect(wine).not_to be_valid
+      expect(wine.errors[:wine]).to include(/can't be blank/)
+    end
+
+    it 'does not require a wine if the status is deleted' do
+      wine = factory(status: :deleted, wine: nil)
+
+      expect(wine).to be_valid
+    end
+  end
+end
