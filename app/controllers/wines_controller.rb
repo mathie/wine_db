70f7d3d6abfc6e805a6ba864d0a8796517d990fe
Graeme Mathieson <mathie@woss.name>
2015-01-18 14:45:08 +0000

    List the wines for a single producer.

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -12,6 +12,9 @@ class WinesController < ApplicationController
     if location_id = params[:location_id]
       @location = Location.find(location_id)
       @location.wines
+    elsif producer_id = params[:producer_id]
+      @producer = Producer.find(producer_id)
+      @producer.wines
     else
       Wine
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   resources :lwin_identifiers, only: [ :index, :show ]
-  resources :producers, only: [ :index, :show ]
+
+  resources :producers, only: [ :index, :show ] do
+    resources :wines, shallow: true, only: [ :index ]
+  end
+
   resources :classifications, only: [ :index, :show ]
+
   resources :locations, only: [ :index, :show ] do
     resources :locations, shallow: true, only: [ :index ]
     resources :wines, shallow: true, only: [ :index ]
   end
+
   resources :wines, only: [ :index, :show ]
 
   root to: 'pages#index'
