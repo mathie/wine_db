98ca16a1be0bd979e871ba87704f16fae70db6d1
Graeme Mathieson <mathie@woss.name>
2015-01-18 14:31:04 +0000

    Allow browsing of wines for a particular location.

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,9 +1,19 @@
 class WinesController < ApplicationController
   def index
-    @wines = Wine.paginated(page)
+    @wines = scope.paginated(page)
   end
 
   def show
     @wine = Wine.find(params.require(:id))
+  end
+
+  private
+  def scope
+    if location_id = params[:location_id]
+      @location = Location.find(location_id)
+      @location.wines
+    else
+      Wine
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :classifications, only: [ :index, :show ]
   resources :locations, only: [ :index, :show ] do
     resources :locations, shallow: true, only: [ :index ]
+    resources :wines, shallow: true, only: [ :index ]
   end
   resources :wines, only: [ :index, :show ]
 
