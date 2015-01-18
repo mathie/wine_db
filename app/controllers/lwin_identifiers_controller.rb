7d6ce71c549c16bb49729a4ffe3bc12eb8d23968
Graeme Mathieson <mathie@woss.name>
2015-01-18 07:45:03 +0000

    Show the detail on an identifier.

--- app/controllers/lwin_identifiers_controller.rb
+++ app/controllers/lwin_identifiers_controller.rb
@@ -2,4 +2,8 @@ class LwinIdentifiersController < ApplicationController
   def index
     @lwin_identifiers = LwinIdentifier.paginated(params[:page])
   end
+
+  def show
+    @lwin_identifier = LwinIdentifier.find(params.require(:id))
+  end
 end
--- spec/controllers/lwin_identifiers_controller_spec.rb
+++ spec/controllers/lwin_identifiers_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe LwinIdentifiersController do
+  let(:lwin_identifiers_class) { class_spy('LwinIdentifier').as_stubbed_const }
 
   describe "GET index" do
-    let(:lwin_identifiers_class) { class_spy('LwinIdentifier').as_stubbed_const }
     let(:lwin_identifiers) { [ instance_spy('LwinIdentifier') ] }
 
     before(:each) do
@@ -26,7 +26,7 @@ RSpec.describe LwinIdentifiersController do
       expect(response).to render_template('index')
     end
 
-    it "asks the view for a paginated list of identifiers" do
+    it "asks the model for a paginated list of identifiers" do
       do_get
 
       expect(lwin_identifiers_class).to have_received(:paginated)
@@ -39,4 +39,39 @@ RSpec.describe LwinIdentifiersController do
     end
   end
 
+  describe "GET show" do
+    let(:lwin_identifier) { instance_spy('LwinIdentifier') }
+
+    before(:each) do
+      allow(lwin_identifiers_class).to receive(:find) { lwin_identifier }
+    end
+
+    def do_get
+      get :show, id: 'uuid'
+    end
+
+    it "returns http success" do
+      do_get
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the show template" do
+      do_get
+
+      expect(response).to render_template('show')
+    end
+
+    it "asks the model for the correct identifier" do
+      do_get
+
+      expect(lwin_identifiers_class).to have_received(:find).with('uuid')
+    end
+
+    it "passes @lwin_identifier on to the view" do
+      do_get
+
+      expect(assigns(:lwin_identifier)).to eq(lwin_identifier)
+    end
+  end
 end
