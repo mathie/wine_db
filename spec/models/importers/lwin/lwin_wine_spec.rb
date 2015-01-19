f46eb030824f7c2a1162a358e05d74ccc6f5b1e1
Graeme Mathieson <mathie@woss.name>
2015-01-19 17:26:16 +0000

    More successful way to stub the consts?

--- spec/models/importers/lwin/lwin_wine_spec.rb
+++ spec/models/importers/lwin/lwin_wine_spec.rb
@@ -1,51 +1,47 @@
 require 'rails_helper'
 
-module Importers
-  module Lwin
-    RSpec.describe LwinWine do
-      describe '#from_row' do
-        context 'for a live wine' do
-          let!(:wine_class) { class_spy('LiveWine').as_stubbed_const }
-          let(:row) { instance_spy('Row', status: :live) }
+RSpec.describe Importers::Lwin::LwinWine do
+  describe '#from_row' do
+    context 'for a live wine' do
+      let!(:wine_class) { class_spy('Importers::Lwin::LiveWine').as_stubbed_const }
+      let(:row) { instance_spy('Row', status: :live) }
 
-          it 'correctly constructs the right wine' do
-            wine = described_class.from_row(row)
+      it 'correctly constructs the right wine' do
+        wine = described_class.from_row(row)
 
-            expect(wine_class).to have_received(:new)
-          end
-        end
+        expect(wine_class).to have_received(:new)
+      end
+    end
 
-        context 'for a deleted wine' do
-          let!(:wine_class) { class_spy('DeletedWine').as_stubbed_const }
-          let(:row) { instance_spy('Row', status: :deleted) }
+    context 'for a deleted wine' do
+      let!(:wine_class) { class_spy('Importers::Lwin::DeletedWine').as_stubbed_const }
+      let(:row) { instance_spy('Row', status: :deleted) }
 
-          it 'correctly constructs the right wine' do
-            wine = described_class.from_row(row)
+      it 'correctly constructs the right wine' do
+        wine = described_class.from_row(row)
 
-            expect(wine_class).to have_received(:new)
-          end
-        end
+        expect(wine_class).to have_received(:new)
+      end
+    end
 
-        context 'for a combined wine' do
-          let!(:wine_class) { class_spy('CombinedWine').as_stubbed_const }
-          let(:row) { instance_spy('Row', status: :combined) }
+    context 'for a combined wine' do
+      let!(:wine_class) { class_spy('Importers::Lwin::CombinedWine').as_stubbed_const }
+      let(:row) { instance_spy('Row', status: :combined) }
 
-          it 'correctly constructs the right wine' do
-            wine = described_class.from_row(row)
+      it 'correctly constructs the right wine' do
+        wine = described_class.from_row(row)
 
-            expect(wine_class).to have_received(:new)
-          end
-        end
+        expect(wine_class).to have_received(:new)
+      end
+    end
 
-        context 'for an unexpected status' do
-          let(:row) { instance_spy('Row', status: :different) }
+    context 'for an unexpected status' do
+      let(:row) { instance_spy('Row', status: :different) }
 
-          it 'correctly constructs a live wine' do
-            expect {
-              described_class.from_row(row)
-            }.to raise_error
-          end
-        end
+      it 'correctly constructs a live wine' do
+        expect {
+          described_class.from_row(row)
+        }.to raise_error
       end
     end
   end
