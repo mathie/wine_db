c246643ce9ec68f706f24fac074da5edeeb2a623
Graeme Mathieson <mathie@woss.name>
2015-01-19 15:11:55 +0000

    PostgreSQL search on each of the types.

--- app/controllers/lwin_identifiers_controller.rb
+++ app/controllers/lwin_identifiers_controller.rb
@@ -1,9 +1,20 @@
 class LwinIdentifiersController < ApplicationController
   def index
-    @lwin_identifiers = LwinIdentifier.paginated(page)
+    @lwin_identifiers = scope.paginated(page)
   end
 
   def show
     @lwin_identifier = LwinIdentifier.find(params.require(:id))
+  end
+
+  private
+  def scope
+    scope = LwinIdentifier
+
+    if query = params[:q]
+      scope.search(query)
+    else
+      scope
+    end
   end
 end
